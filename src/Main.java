fc9cba2b685fdacf3d2669fcc84f926c6ecd2935
Maxim Gerasimovich <mgerasimovich@mail.ru>
2024-10-11 19:23:22 +0300

    main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Arithmetic mainTest = new Arithmetic(100, 200);
+        Arithmetic mainTest = new Arithmetic(200, 300);
     }
 }
